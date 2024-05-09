c47709f095e7429c70f5abd76faf59a71af130e7
Aditya Patel <adityapatel8912@gmail.com>
2024-05-10 04:56:50 +0530

    Created thunderclient collection to manage request

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,28 +1,39 @@
 const express = require('express');
 const User = require('../models/User');
-const { body, validationResult } = require('express-validator');
+const { body, validationResult, Result } = require('express-validator');
 const router = express.Router();
 
-// Create a User using: POST "api/auth/". Doesn't require Auth
-router.post('/', [
+// Create a User using: POST "api/auth/ceateuser". No login required
+router.post('/createuser', [
     body('name', "Enter valid name").isLength({ min: 3 }),
     body('email', "Enter valid email").isEmail(),
     body('password', "Enter valid password").isLength({ min: 5 }),
-], (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    }).then(() => res.json({status: 'success', msg: 'User created successfully in DB!', error:null}))
+], async (req, res) => {
+    try {    
+        // validation of data
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });    // to response bad request and msg 
+        }
     
-    .catch(err => {console.log(err)
-
-    res.json({error: 'Enter valid data', msg: err.message})});
-    console.log(req.body);
+        // checking the user exists with the same email
+        let isExists = await User.findOne({email: req.body.email});
+        if (isExists) {
+            return res.status(400).json({status: "User exists", msg: "The user already exists with this mail."})
+        }
+    
+        let user = await User.create({
+            name: req.body.nam,
+            email: req.body.email,
+            password: req.body.password
+        });
+        
+        res.json({status: "User created!"});
+        console.log(user);
+    } catch (error) {
+        res.send('Some error occured!');
+        console.log(error.message);
+    }
 });
 
 module.exports = router;
